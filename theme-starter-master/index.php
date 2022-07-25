59279d35218a2ff836347a903cab1e935e2152c4
DominikLachowski <dominik.lachowski@tlen.pl>
2022-07-25 16:53:01 +0200

    commit indexphp

--- theme-starter-master/index.php
+++ theme-starter-master/index.php
@@ -2,11 +2,25 @@
 get_header();
 ?>
 
-<?php the_content(); ?>
 
-<?php
-echo esc_html( get_template_part( 'template-parts/pagination', 'infinite-scroll' ) );
-?>
+	<section class="LoginPage">
+		<div class="LoginPage__container">
+
+			<h1>Log in </h1>
+
+			<p>Log in with your data that you received from the photographer</p>
+			<p>heyo123</p>
+
+			<form>
+				<input class="LoginPage__container--email" type="text" placeholder="E-mail">
+				<input class="LoginPage__container--password" type="text" placeholder="Password">
+			</form>
+		<div class="LoginPage__container__link--button">
+			<a class="LoginPage__container__link--ForgotPassword" href="#">Forgot password?</a>
+			<button type="submit">Log in</button>
+		</div>
+		</div>
+	</section>
 
 <?php
 get_footer();
